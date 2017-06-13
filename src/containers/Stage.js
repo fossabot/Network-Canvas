07bf8e07149e66fb6fc16c512c1d0bb7935f64e4
Steve Mckellar <steve@wwqrd.uk>
2017-06-13 15:46:30 +0100

    Consistent transition naming

--- src/containers/Stage.js
+++ src/containers/Stage.js
@@ -29,7 +29,7 @@ class Stage extends Component {
 
     return (
       <CSSTransitionGroup
-        transitionName="stage-transition"
+        transitionName="stage--transition"
         transitionEnterTimeout={styles.animation.duration.slow * 2}
         transitionLeaveTimeout={styles.animation.duration.slow}
         transitionAppear
